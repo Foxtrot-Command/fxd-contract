a0acb87915937534914c8bcb5ba62a4c66a36a11
Michydev <michyaraque@gmail.com>
2022-03-31 15:46:58 +0200

    fix: :fire: Deploy fixed and extra files removed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,56 +11,11 @@ async function main() {
   [masterAccount] = await ethers.getSigners();
 
   const FoxtrotCommandToken = await hre.ethers.getContractFactory("FoxtrotCommand");
-  const foxtrotToken = await FoxtrotCommandToken.deploy(215000000, [
-    "Seed",
-    "Private",
-    "Public",
-    "Ecosystem",
-    "Partners",
-    "Team",
-    "Staking",
-    "Play",
-    "Marketing"
-  ], [
-    ethers.utils.parseEther(String(32250000)),
-    ethers.utils.parseEther(String(31605000)),
-    ethers.utils.parseEther(String(4945000)),
-    ethers.utils.parseEther(String(19350000)),
-    ethers.utils.parseEther(String(19350000)),
-    ethers.utils.parseEther(String(32250000)),
-    ethers.utils.parseEther(String(43000000)),
-    ethers.utils.parseEther(String(25800000)),
-    ethers.utils.parseEther(String(6450000))
-  ])
-  
-  //const BusdToken = await hre.ethers.getContractFactory("MockBUSD");
-  //const busdToken = await BusdToken.deploy();
-  
-  //const SeedSale = await hre.ethers.getContractFactory("FoxtrotSeedSale");
-  //const seedSale = await SeedSale.deploy(masterAccount.address, busdToken.address);
-
+  const foxtrotToken = await FoxtrotCommandToken.deploy()
   await foxtrotToken.deployed();
-  //await busdToken.deployed();
-  //await seedSale.deployed();
-
-  //await seedSale.connect(masterAccount).addAddressToWhitelist(masterAccount.address, ethers.utils.parseEther('120000'));
-  //await foxtrotToken.connect(masterAccount).transfer(seedSale.address, ethers.utils.parseEther('32250000'));
-
-  //await foxtrotToken.connect(masterAccount).setAddressOfAllowedContract('Seed', seedSale.address);
-  //await foxtrotToken.connect(masterAccount).safeTransferBusinessTokens('Seed');
-  //await seedSale.setContractToken(foxtrotToken.address);
-
   console.log("Token deployed to:", foxtrotToken.address);
-
-  /* console.log(`
-NEXT_PUBLIC_SEED_CONTRACT=${seedSale.address}
-NEXT_PUBLIC_BUSD_CONTRACT=${busdToken.address}
-NEXT_PUBLIC_FXD_CONTRACT=${foxtrotToken.address}
-  `) */
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
