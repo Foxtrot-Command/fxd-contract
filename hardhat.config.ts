a220a5ffdc3145d0d2229e17f1cdd426dca0dcca
Michydev <michyaraque@gmail.com>
2022-04-20 00:27:01 +0200

    feat: :sparkles: upgrade from js to typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,24 +1,15 @@
-require("dotenv").config();
 
-require("@nomiclabs/hardhat-etherscan");
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
-require("solidity-coverage");
-require('@openzeppelin/hardhat-upgrades');
-require('hardhat-contract-sizer');
+import * as dotenv from "dotenv";
+dotenv.config()
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "hardhat-gas-reporter";
+import "hardhat-deploy";
+import "./tasks";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
