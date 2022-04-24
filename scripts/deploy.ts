dcd2afe6fc06f713f19dc3b338149f13bd9863ff
Michydev <michyaraque@gmail.com>
2022-04-25 01:20:14 +0200

    chore: :sparkles: update deploy function with the multisig address

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,20 +7,20 @@ import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import hre from 'hardhat'
 
 async function main() {
-  let deployer: SignerWithAddress,
-    multisignAccount: SignerWithAddress;
+  let deployer: SignerWithAddress;
 
-  [deployer, multisignAccount] = (hre as any).customSigners.concat(await hre.ethers.getSigners());
+  [deployer] = (hre as any).customSigners.concat(await hre.ethers.getSigners());
+  let gnosisSafeAddress = '0x8234CdBaA9F5c23bE1bd966C673A3D9f4096AcC7';
 
   const FoxtrotCommandToken = await hre.ethers.getContractFactory("FoxtrotCommand");
-  const foxtrotToken = await FoxtrotCommandToken.deploy(multisignAccount.address)
+  const foxtrotToken = await FoxtrotCommandToken.deploy(gnosisSafeAddress)
   let contract = await foxtrotToken.deployed();
   console.log("Token deployed to:", foxtrotToken.address);
 
   await hre.run("verify:verify", {
     address: contract,
     contract: "FoxtrotCommand",
-    constructorArguments: [multisignAccount.address]
+    constructorArguments: [gnosisSafeAddress]
   });
 }
 
